aedcd87f87e0303abc606e4ff5a4fad7888d47e4
Luohuayu <i@qqq.moe>
2022-04-07 03:17:36 +0800

    实现构建

--- fmlloader/src/main/java/FoxServerLauncher.java
+++ fmlloader/src/main/java/FoxServerLauncher.java
@@ -1,6 +1,8 @@
-package catserver;
-
 public class FoxServerLauncher {
+    public static void main(String[] args) throws Throwable {
+        Class.forName("net.minecraftforge.fml.server.ServerMain", true, ClassLoader.getSystemClassLoader()).getMethod("main", String[].class).invoke(null, args);
+    }
+
     public static String getVersion() {
         return (FoxServerLauncher.class.getPackage().getImplementationVersion() != null) ? FoxServerLauncher.class.getPackage().getImplementationVersion() : "unknown";
     }
