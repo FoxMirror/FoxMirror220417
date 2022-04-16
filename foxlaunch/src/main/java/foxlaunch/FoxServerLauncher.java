6b4717d3af463ff79b44e664705615a80f1013e2
Luohuayu <i@qqq.moe>
2022-04-16 20:30:11 +0800

    校验Java版本

--- foxlaunch/src/main/java/foxlaunch/FoxServerLauncher.java
+++ foxlaunch/src/main/java/foxlaunch/FoxServerLauncher.java
@@ -1,7 +1,11 @@
 package foxlaunch;
 
+import java.net.URLClassLoader;
+
 public class FoxServerLauncher {
     public static void main(String[] args) throws Throwable {
+        checkJavaVersion();
+
         System.out.println("Loading libraries, please wait...");
 
         DataManager.setupLibrariesMap();
@@ -10,5 +14,24 @@ public class FoxServerLauncher {
         DataManager.downloadLibraries();
 
         DataManager.generateLaunchScript(args);
+    }
+
+    private static void checkJavaVersion() throws Exception {
+        String javaVersion = System.getProperty("java.version", "");
+
+        if (FoxServerLauncher.class.getClassLoader() instanceof URLClassLoader) {
+            System.out.println(String.format(LanguageUtils.I18nToString("launch.java_wrong"), javaVersion));
+            System.exit(1);
+        }
+
+        String[] javaVersionSplit = javaVersion.split("\\.");
+        try {
+            if (javaVersionSplit.length >= 3 && Integer.parseInt(javaVersionSplit[0]) >= 17) {
+                return;
+            }
+        } catch (Exception ignored) {}
+
+        System.out.println(String.format(LanguageUtils.I18nToString("launch.java_wrong"), javaVersion));
+        Thread.sleep(5000);
     }
 }
